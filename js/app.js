9be3c5c02668bf3ba6d6f22c3f4004023f9910af
therealmensumit <sk03950@gmail.com>
2025-01-28 11:20:09 +0530

    text reveal animation

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log(gsap);
+// console.log(gsap);
 
 gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline();
@@ -41,6 +41,8 @@ if (screen.width > 768) {
   splitText.forEach((e, i) => {
     if (i < halfLength) {
       text.innerHTML += `<span class="x">${e}</span>`;
+    } else if (e === " ") {
+      text.innerHTML += `<span class="x">&nbsp;</span>`;
     } else {
       text.innerHTML += `<span class="y">${e}</span>`;
     }
@@ -97,6 +99,34 @@ if (screen.width > 768) {
       start: "top top",
       scrub: 2,
       pin: true,
+    },
+  });
+
+  // text reveal animation
+  const revealText = document.getElementById("reveal-text");
+  const rText = revealText.innerText;
+  const splitRevealText = rText.split("");
+  revealText.innerHTML = "";
+  splitRevealText.forEach((e) => {
+    if (e === " ") {
+      revealText.innerHTML += `<span class="single-text">&nbsp;</span>`;
+    } else {
+      revealText.innerHTML += `<span class="single-text">${e}</span>`;
+    }
+  });
+
+  // GSAP animation with ScrollTrigger
+  gsap.to(".single-text", {
+    y: -10,
+    opacity: 1, // Start with text hidden
+    stagger: 0.05, // Add stagger for the animation
+    scrollTrigger: {
+      trigger: revealText,
+      start: "top 70%", // Trigger when the top of the element reaches 90% of the viewport
+      end: "bottom 30%", // End the animation when the bottom of the element reaches 10% of the viewport
+      scrub: true, // Smooth animation that is linked to the scroll
+      // pin: true,
+      // markers: true,
     },
   });
 }
